1522a611c5a5996fb1b337ac8c9528acce965167
vctorr1 <128295802+vctorr1@users.noreply.github.com>
2024-07-23 11:32:13 +0200

    Proyecto creado

---
+++ src/main/java/victor/microservicios/microservicios1/Microservicios1Application.java
@@ -0,0 +1,13 @@
+package victor.microservicios.microservicios1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservicios1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservicios1Application.class, args);
+	}
+
+}
---
+++ src/test/java/victor/microservicios/microservicios1/Microservicios1ApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.microservicios.microservicios1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicios1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
